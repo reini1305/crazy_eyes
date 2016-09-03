74cc6e551035aefd03b562f6ef95447017a889da
Christian Reinbacher <christian.reinbacher@gmx.at>
2016-09-03 22:00:50 +0200

    added support for quick view

--- src/crazy_eyes.c
+++ src/crazy_eyes.c
@@ -60,7 +60,7 @@ static void update_color() {
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
-  
+
   // Read preferences
   Tuple *t = dict_find(iter, MESSAGE_KEY_eyebrows);
   if(t) {
@@ -126,10 +126,10 @@ static void blink_down_callback(void* data){
 static void googly_update_countdown(void* data){
   // read accelerometer data
   accel_service_peek(&googly_data);
- 
+
   // super simple mode
   //googly_angle = atan2_lookup(googly_data.x,googly_data.y);
-  
+
   // linear acceleration
   int32_t goal_angle = atan2_lookup(googly_data.x,googly_data.y);
   if((abs(googly_angle-goal_angle) < GOOGLY_ACCEL_INCREMENT) && abs(googly_acceleration) < GOOGLY_ACCEL_INCREMENT) {
@@ -165,21 +165,21 @@ static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
 }
 
 static void hands_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
+  GRect bounds = layer_get_unobstructed_bounds(layer);
   GPoint center = grect_center_point(&bounds);
   GColor color= PBL_IF_COLOR_ELSE((GColor8){.argb=((uint8_t)(0xC0|background_color))},GColorBlack);
-  
+
   const int16_t pupil_center_dist = eye_radius - pupil_radius - 4;
   const int16_t offset = (watch_info_get_model() == WATCH_INFO_MODEL_PEBBLE_STEEL) ? 12:0;
-  
+
   GPoint left_eye_center = center;
   GPoint right_eye_center = center;
-  
+
   left_eye_center.x-=eye_radius+eye_distance;
   right_eye_center.x+=eye_radius+eye_distance;
   left_eye_center.y+=offset;
   right_eye_center.y+=offset;
-  
+
   // draw the eye circles
   graphics_context_set_fill_color(ctx, color);
 
@@ -195,7 +195,7 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
   graphics_draw_circle(ctx,left_eye_center, eye_radius+1);
   graphics_draw_circle(ctx,right_eye_center, eye_radius+1);
 #endif
-  
+
   // calculate the center of the hour and minute circles (pupils)
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
@@ -218,13 +218,13 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_circle(ctx,hour_center, pupil_radius);
   graphics_fill_circle(ctx,minute_center, pupil_radius);
-  
+
   graphics_context_set_fill_color(ctx, GColorWhite);
   hour_center.x+=2;hour_center.y-=2;
   minute_center.x+=2;minute_center.y-=2;
   graphics_fill_circle(ctx,hour_center, 2);
   graphics_fill_circle(ctx,minute_center, 2);
-  
+
   // Draw the blinking
   const GPathInfo BOLT_PATH_INFO = {
     .num_points = 4,
@@ -236,13 +236,13 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
     .y = (int16_t) left_eye_center.y - 2 * eye_radius + blink_y - 8,
   };
   gpath_move_to(s_my_path_ptr, blink_center);
-  
+
   // Fill the path:
   graphics_context_set_fill_color(ctx, color);
 
   gpath_draw_filled(ctx, s_my_path_ptr);
   gpath_destroy(s_my_path_ptr);
-  
+
   if(s_settings.show_eyebrows) {
     // Draw the eyebrows
     int16_t charge_diff = (s_settings.angryness == 11) ? ((charge_percentage /5) -10) : (s_settings.angryness*2-10);
@@ -252,12 +252,12 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
       .x = (int16_t) left_eye_center.x-eye_radius+bluetooth_diff_x,
       .y = (int16_t) left_eye_center.y-eye_radius-20-charge_diff+bluetooth_diff_y,
     };
-    
+
     GPoint right = {
       .x = (int16_t) left_eye_center.x+eye_radius,
       .y = (int16_t) left_eye_center.y-eye_radius-20+charge_diff,
     };
-    
+
     graphics_draw_line(ctx,left,right);
 #ifndef PBL_COLOR
     left.y--;right.y--;
@@ -267,14 +267,14 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
     left.y = (int16_t) right_eye_center.y-eye_radius-20+charge_diff;
     right.x = (int16_t) right_eye_center.x+eye_radius-bluetooth_diff_x;
     right.y = (int16_t) right_eye_center.y-eye_radius-20-charge_diff+bluetooth_diff_y,
-    
+
     graphics_draw_line(ctx,left,right);
 #ifndef PBL_COLOR
     left.y--;right.y--;
     graphics_draw_line(ctx,left,right);
 #endif
   }
-  
+
   if(s_settings.show_mouth)
   {
     // Draw the mouth (with weekdays)
@@ -283,7 +283,7 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
     mouth.size.w*=7;
     mouth.size.h = 20;
     mouth.origin.x = (bounds.size.w - mouth.size.w)/2;
-    mouth.origin.y = left_eye_center.y + eye_radius + 15;
+    mouth.origin.y = left_eye_center.y + eye_radius + 10;
     graphics_context_set_fill_color(ctx,GColorWhite);
     graphics_context_set_stroke_color(ctx,GColorBlack);
 
@@ -350,23 +350,23 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  
+
   // init hands
   hands_layer = layer_create(bounds);
   layer_set_update_proc(hands_layer, hands_update_proc);
   layer_add_child(window_layer, hands_layer);
-  
+
   accel_tap_service_subscribe(accel_tap_handler);
-  
+
   // force update
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
   handle_tick(t, MINUTE_UNIT);
   tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
-  
+
   bluetooth_connection_service_subscribe(handle_bluetooth);
   handle_bluetooth(bluetooth_connection_service_peek());
-  
+
   battery_state_service_subscribe(handle_battery);
   handle_battery(battery_state_service_peek());
 }
@@ -382,21 +382,21 @@ static void init(void) {
   loadSettings();
   app_message_open(100,100);
   app_message_register_inbox_received(in_received_handler);
-  
+
   nightstand_window_init();
   window = window_create();
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
-  
+
   googly_mode=false;
   googly_angle = 0;
   googly_acceleration = 0;
   // Push the window onto the stack
   const bool animated = true;
   window_stack_push(window, animated);
-  
+
 }
 
 static void deinit(void) {
@@ -410,4 +410,3 @@ int main(void) {
   app_event_loop();
   deinit();
 }
-
